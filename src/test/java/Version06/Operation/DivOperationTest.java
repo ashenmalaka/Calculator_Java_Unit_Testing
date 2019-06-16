641d6119a9d6e8f797a72cac7b5f522d94278979
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-06-17 01:45:32 +0530

    Added codes for unit testing in division

--- src/test/java/Version06/Operation/DivOperationTest.java
+++ src/test/java/Version06/Operation/DivOperationTest.java
@@ -1,7 +1,52 @@
 package Version06.Operation;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class DivOperationTest {
+
+    @Test
+    public void divPositiveByPositiveTest() {
+        Operation operation = new DivOperation(6, 3);
+        double result = operation.perform();
+        assertThat(result, is(2.0));
+    }
+
+    @Test
+    public void divPositiveByZeroTest() {
+        Operation operation = new DivOperation(6, 0);
+        double result = operation.perform();
+        assertThat(result, is(0.0));
+    }
+
+    @Test
+    public void divNegativeByZeroTest() {
+        Operation operation = new DivOperation(-6, 0);
+        double result = operation.perform();
+        assertThat(result, is(0.0));
+    }
+
+    @Test
+    public void divNegativeByNegativeTest() {
+        Operation operation = new DivOperation(-6, -3);
+        double result = operation.perform();
+        assertThat(result, is(2.0));
+    }
+
+    @Test
+    public void divZeroByZeroTest() {
+        Operation operation = new DivOperation(0, 0);
+        double result = operation.perform();
+        assertThat(result, is(0.0));
+    }
+
+    @Test
+    public void DivPositiveByNegativeTest() {
+        Operation operation = new AddOperation(6, -3);
+        double result = operation.perform();
+        assertThat(result, is(-2.0));
+    }
 
 }
